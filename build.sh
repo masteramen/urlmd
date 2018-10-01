8e481d33e5e955410c63cff9705a3f8cdfd1c1bb
alexwang <2919798881@qq.com>
2018-10-01 12:25:01 +0800

    auto commit 2018年10月 1日 星期一 12时25分01秒 CST

--- build.sh
+++ build.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
-git cd jekyll
-git pull
-git commit -am "auto commit"
-git push
 
-if [ "$?" -ne "0" ]; then  
-    echo "usage: $0 <area> <hours>"  
-    exit 2  
-fi  
 
-JEKYLL_ENV=production jekyll build --config jekyll/_config_local.yml -s jekyll -d site  
+cd jekyll
+git pull || (echo "fail to pull jekyll $(date)" && exit 1)
+git commit -am "auto commit $(date)"  || (echo "fail to commit jekyll $(date)" && exit 1)
+git push  || (echo "fail to push jekyll $(date)" && exit 1)
+
+
+cd ..
+
+JEKYLL_ENV=production jekyll build --config jekyll/_config_local.yml -s jekyll -d site || (echo "fail to build jekyll $(date)" && exit 1)
+
+cd site
+git pull || (echo "fail to pull site $(date)" && exit 1)
+git commit -am "auto commit $(date)"  || (echo "fail to commit site $(date)" && exit 1)
+git push  || (echo "fail to push site $(date)" && exit 1)
+cd ..
+echo "success build and push site"
 exit 0
