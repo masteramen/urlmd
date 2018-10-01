07c4dd5a649e9c486161a3c3e4b9bbfcea680f53
alexwang <2919798881@qq.com>
2018-10-01 12:00:01 +0800

    auto commit

--- build.sh
+++ build.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
+git cd jekyll
+git pull
+git commit -am "auto commit"
+git push
+
+if [ "$?" -ne "0" ]; then  
+    echo "usage: $0 <area> <hours>"  
+    exit 2  
+fi  
+
 JEKYLL_ENV=production jekyll build --config jekyll/_config_local.yml -s jekyll -d site  
-exit(0)
+exit 0
